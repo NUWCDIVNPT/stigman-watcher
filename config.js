6d465f5a430d582b2372d977c26f5d28f1f76e91
csmig <csmig@csmig.com>
2021-04-10 23:44:53 -0400

    wip

--- config.js
+++ config.js
@@ -1,97 +1,100 @@
-const yargs = require('yargs')
+// const yargs = require('yargs')
+const options = require('./lib/args')
 const fs = require('fs')
 const log = require('./lib/logger')
 
-const options = yargs
-  .usage('\nUsage: $0 [options]')
-  .option('e', {
-    alias: 'env',
-    describe: 'Path to an environment file (default: ./.env)\n'
-  })
-  .option('p', {
-    alias: 'path',
-    describe: 'The path to watch'
-  })
-  .option('c', {
-    alias: 'collection-id',
-    describe: 'The STIG Manager collectionId to manage\n'
-  })
-  .option('s', {
-    alias: 'silent',
-    describe: 'Disable logging to the console'
-  })
-  .option('log-level', {
-    describe: 'Log level (0-5) for the console\n0 = error\n1 = warn\n2 = info (default)\n3 = http\n4 = verbose\n5 = debug'
-  })
-  .option('log-file', {
-    describe: 'Path to the log file (default: ./watcher.log)'
-  })
-  .option('log-file-level', {
-    describe: 'Log level (0-5) for the log file (default: 4)'
-  })
-  .option('no-log-file', {
-    describe: 'Disable logging to a logfile\n'
-  })
-  .option('api', {
-    describe: 'The base URL of the STIG Manager API.'
-  })
-  .option('authority', {
-    describe: 'The base URL of the OIDC authority.'
-  })
-  .option('client-id', {
-    describe: 'OIDC clientId to authenticate'
-  })
-  .option('client-key', {
-    describe: 'Path to a PEM encoded private key\n'
-  })
-  .option('add-existing', {
-    describe: 'Existing files are processed first'
-  })
-  .option('cargo-delay', {
-    describe: 'Milliseconds to delay processing the queue\n(default: 2000)'
-  })
-  .option('cargo-size', {
-    describe: 'Maximum queue size that triggers processing\n(default: 25)'
-  })
-  .option('no-create', {
-    describe: 'Disable creating Assets or STIG Assignments'
-  })
-  .option('ignored-dirs', {
-    describe: 'CSV of directory names to ignore'
-  })
-  .option('disable-polling', {
-    describe: 'Use file system events without polling'
-  })
-  .option('stability-threshold', {
-    describe: 'Milliseconds to allow file size to stabilize\nbefore processing'
-  })
-  .argv
+// const options2 = yargs
+//   .usage('\nUsage: $0 [options]')
+//   .env('WATCHER')
+//   .option('e', {
+//     alias: 'env',
+//     describe: 'Path to an environment file (default: ./.env)\n'
+//   })
+//   .option('p', {
+//     alias: 'path',
+//     describe: 'The path to watch'
+//   })
+//   .option('c', {
+//     alias: 'collection-id',
+//     describe: 'The STIG Manager collectionId to manage\n'
+//   })
+//   .option('s', {
+//     alias: 'silent',
+//     describe: 'Disable logging to the console'
+//   })
+//   .option('log-level', {
+//     describe: 'Log level (0-5) for the console\n0 = error\n1 = warn\n2 = info (default)\n3 = http\n4 = verbose\n5 = debug'
+//   })
+//   .option('log-file', {
+//     describe: 'Path to the log file (default: ./watcher.log)'
+//   })
+//   .option('log-file-level', {
+//     describe: 'Log level (0-5) for the log file (default: 4)'
+//   })
+//   .option('no-log-file', {
+//     describe: 'Disable logging to a logfile\n'
+//   })
+//   .option('api', {
+//     describe: 'The base URL of the STIG Manager API.'
+//   })
+//   .option('authority', {
+//     describe: 'The base URL of the OIDC authority.'
+//   })
+//   .option('client-id', {
+//     describe: 'OIDC clientId to authenticate'
+//   })
+//   .option('client-key', {
+//     describe: 'Path to a PEM encoded private key\n'
+//   })
+//   .option('add-existing', {
+//     describe: 'Existing files are processed first',
+//     type: 'boolean'
+//   })
+//   .option('cargo-delay', {
+//     describe: 'Milliseconds to delay processing the queue\n(default: 2000)'
+//   })
+//   .option('cargo-size', {
+//     describe: 'Maximum queue size that triggers processing\n(default: 25)'
+//   })
+//   .option('no-create', {
+//     describe: 'Disable creating Assets or STIG Assignments'
+//   })
+//   .option('ignored-dirs', {
+//     describe: 'CSV of directory names to ignore'
+//   })
+//   .option('disable-polling', {
+//     describe: 'Use file system events without polling'
+//   })
+//   .option('stability-threshold', {
+//     describe: 'Milliseconds to allow file size to stabilize\nbefore processing'
+//   })
+//   .argv
 
-if (options.env && !fs.existsSync(options.env)) {
-  log('error', {
-    component: 'watcher',
-    message: `Environment file not found`,
-    path: options.env
-  })
-}
-else if (!options.env && fs.existsSync('.env')) {
-  options.env = '.env'
-}
-if (options.env) {
-  const dotenv = require('dotenv')
-  dotenv.config({ path: options.env });
-}
+// if (options.env && !fs.existsSync(options.env)) {
+//   log('error', {
+//     component: 'watcher',
+//     message: `Environment file not found`,
+//     path: options.env
+//   })
+// }
+// else if (!options.env && fs.existsSync('.env')) {
+//   options.env = '.env'
+// }
+// if (options.env) {
+//   const dotenv = require('dotenv')
+//   dotenv.config({ path: options.env });
+// }
 
-let logLevel = 'info'
-if (options.verbose) {
-  if (options.verbose >= 3) {
-    logLevel = 'silly'
-  }
-  else if (options.verbose == 2) {
-    logLevel = 'verbose'
-  }
-  console.log(`Verbosity: ${options.verbose}`)
-}
+// let logLevel = 'info'
+// if (options.verbose) {
+//   if (options.verbose >= 3) {
+//     logLevel = 'silly'
+//   }
+//   else if (options.verbose == 2) {
+//     logLevel = 'verbose'
+//   }
+//   console.log(`Verbosity: ${options.verbose}`)
+// }
 
 let ignoreDirs
 if (process.env.WATCHER_IGNORE_DIRS) {
--- index.js
+++ index.js
@@ -1,147 +1,151 @@
 #!/usr/bin/env node
 
-const config = require('./config')
-const logger  = require('./lib/logger')
-const auth = require('./lib/auth')
-const api = require('./lib/api')
-const cargo = require('./lib/cargo')
-const chokidar = require('chokidar');
-const fs = require('fs').promises
-const parsers = require('./lib/parsers')
-const Queue = require('better-queue')
-const {serializeError} = require('serialize-error') 
-
-const cargoQueue = new Queue(cargo.cklsHandler, {
-  id: 'file',
-  batchSize: config.cargoSize,
-  batchDelay: config.cargoDelay,
-  // batchDelayTimeout: config.cargoDelay
-})
-cargoQueue.on('batch_failed', (taskId, err, stats) => {
-  logger.error( {
-    component: 'queue',
-    message: err.message,
-    taskId: taskId,
-    stats: stats
-  })
-})
-
-const parseQueue = new Queue (parseFile, {
-  concurrent: 3
-})
-
-run()
-
-async function parseFile (file, cb) {
-  const component = 'parser'
-  try {
-    const extension = file.substring(file.lastIndexOf(".") + 1)
-    let parser, type
-    if (extension.toLowerCase() === 'ckl') {
-      parser = parsers.reviewsFromCkl
-      type = 'CKL'
-    }
-    else if (extension.toLowerCase() === 'xml') {
-      parser = parsers.reviewsFromScc
-      type = "XCCDF"
-    }
-    else {
-      logger.error({
-        component: component,
-        message: `Ignored unknown extension`,
-        file: file
-      })
-      return false
-    }
-    const data = await fs.readFile(file)
-    logger.info({
-      component: component,
-      message: `Start parse`,
-      file: file
-    })
-    let parseResult = parser(data)
-    parseResult.file = file
-    logger.info({
-      component: component,
-      message: `Queue parsed results`,
-      file: file
-    })
-    cargoQueue.push( parseResult )
-  }
-  catch (e) {
-    logger.error({
-      component: component,
-      message: e.message,
-      file: file
-    })
-    cb( e, undefined)
-  }
-  finally {
-    cb()
-  }
+const {logger} = require('./lib/logger')
+const config = require('./lib/args')
+if (!config) {
+  logger.end()
 }
+// const logger  = require('./lib/logger')
+// const auth = require('./lib/auth')
+// const api = require('./lib/api')
+// const cargo = require('./lib/cargo')
+// const chokidar = require('chokidar');
+// const fs = require('fs').promises
+// const parsers = require('./lib/parsers')
+// const Queue = require('better-queue')
+// const {serializeError} = require('serialize-error') 
+
+// const cargoQueue = new Queue(cargo.cklsHandler, {
+//   id: 'file',
+//   batchSize: config.cargoSize,
+//   batchDelay: config.cargoDelay,
+//   // batchDelayTimeout: config.cargoDelay
+// })
+// cargoQueue.on('batch_failed', (taskId, err, stats) => {
+//   logger.error( {
+//     component: 'queue',
+//     message: err.message,
+//     taskId: taskId,
+//     stats: stats
+//   })
+// })
+
+// const parseQueue = new Queue (parseFile, {
+//   concurrent: 3
+// })
+
+// run()
+
+// async function parseFile (file, cb) {
+//   const component = 'parser'
+//   try {
+//     const extension = file.substring(file.lastIndexOf(".") + 1)
+//     let parser, type
+//     if (extension.toLowerCase() === 'ckl') {
+//       parser = parsers.reviewsFromCkl
+//       type = 'CKL'
+//     }
+//     else if (extension.toLowerCase() === 'xml') {
+//       parser = parsers.reviewsFromScc
+//       type = "XCCDF"
+//     }
+//     else {
+//       logger.error({
+//         component: component,
+//         message: `Ignored unknown extension`,
+//         file: file
+//       })
+//       return false
+//     }
+//     const data = await fs.readFile(file)
+//     logger.info({
+//       component: component,
+//       message: `Start parse`,
+//       file: file
+//     })
+//     let parseResult = parser(data)
+//     parseResult.file = file
+//     logger.info({
+//       component: component,
+//       message: `Queue parsed results`,
+//       file: file
+//     })
+//     cargoQueue.push( parseResult )
+//   }
+//   catch (e) {
+//     logger.error({
+//       component: component,
+//       message: e.message,
+//       file: file
+//     })
+//     cb( e, undefined)
+//   }
+//   finally {
+//     cb()
+//   }
+// }
 
 
-async function run() {
-  try {
-    const token = await auth.getToken( true )
-    logger.info({ component: 'api', message: `STIG Manager API preflight to ${config.apiBase} for Collection ${config.collectionId}`})
-    const assets = await api.getCollectionAssets(config.collectionId)
-    logger.info({ component: 'api', message: `Preflight succeeded, got Assets in Collection ${config.collectionId}`})
-    logger.info({ component: 'api', message: `STIG Manager API preflight for installed STIGs`})
-    const stigs = await api.getInstalledStigs()
-    logger.info({ component: 'api', message: `Preflight succeeded, got installed STIGs`})
+// async function run() {
+//   try {
+//     const token = await auth.getToken( true )
+//     logger.info({ component: 'api', message: `STIG Manager API preflight to ${config.apiBase} for Collection ${config.collectionId}`})
+//     const assets = await api.getCollectionAssets(config.collectionId)
+//     logger.info({ component: 'api', message: `Preflight succeeded, got Assets in Collection ${config.collectionId}`})
+//     logger.info({ component: 'api', message: `STIG Manager API preflight for installed STIGs`})
+//     const stigs = await api.getInstalledStigs()
+//     logger.info({ component: 'api', message: `Preflight succeeded, got installed STIGs`})
 
-    const watcher = chokidar.watch(config.watchDir, {
-      ignored: config.ignoreDirs,
-      ignoreInitial: !config.addExisting,
-      persistent: true,
-      usePolling: config.usePolling
-    })
+//     const watcher = chokidar.watch(config.watchDir, {
+//       ignored: config.ignoreDirs,
+//       ignoreInitial: !config.addExisting,
+//       persistent: true,
+//       usePolling: config.usePolling
+//     })
 
-    watcher.on('error', e => {
-      logger.error({
-        component: 'watcher',
-        error: serializeError(e)
-      })
-    })
+//     watcher.on('error', e => {
+//       logger.error({
+//         component: 'watcher',
+//         error: serializeError(e)
+//       })
+//     })
 
-    watcher.on('add', file  => {
-      // chokidar glob argument doesn't work for UNC Windows, so we check file extension here
-      const extension = file.substring(file.lastIndexOf(".") + 1)
-      if (extension.toLowerCase() === 'ckl') {
-        logger.info({
-          component: 'watcher',
-          message: 'File system event',
-          event:  'add',
-          file: file
-        })
-        parseQueue.push( file )
-      }
-    })
-    logger.info({ component: 'watcher', message: `Starting to watch ${config.watchDir}` })
-  }
-  catch (e) {
-    const errorObj = {
-      component: e.component || 'watcher',
-      message: e.message,
-    }
-    if (e.request) {
-      errorObj.request = {
-        method: e.request.options?.method,
-        url: e.request.requestUrl
-      }
-    }
-    if (e.response) {
-      errorObj.response = {
-        status: e.response.statusCode,
-        body: e.response.body
-      }
-    }
-    if (e.name !== 'RequestError' && e.name !== 'HTTPError') {
-      errorObj.error = serializeError(e)
-    }
-    logger.error(errorObj)
-    process.exit(1)
-  }
-}
+//     watcher.on('add', file  => {
+//       // chokidar glob argument doesn't work for UNC Windows, so we check file extension here
+//       const extension = file.substring(file.lastIndexOf(".") + 1)
+//       if (extension.toLowerCase() === 'ckl') {
+//         logger.info({
+//           component: 'watcher',
+//           message: 'File system event',
+//           event:  'add',
+//           file: file
+//         })
+//         parseQueue.push( file )
+//       }
+//     })
+//     logger.info({ component: 'watcher', message: `Starting to watch ${config.watchDir}` })
+//   }
+//   catch (e) {
+//     const errorObj = {
+//       component: e.component || 'watcher',
+//       message: e.message,
+//     }
+//     if (e.request) {
+//       errorObj.request = {
+//         method: e.request.options?.method,
+//         url: e.request.requestUrl
+//       }
+//     }
+//     if (e.response) {
+//       errorObj.response = {
+//         status: e.response.statusCode,
+//         body: e.response.body
+//       }
+//     }
+//     if (e.name !== 'RequestError' && e.name !== 'HTTPError') {
+//       errorObj.error = serializeError(e)
+//     }
+//     logger.error(errorObj)
+//     process.exit(1)
+//   }
+// }
