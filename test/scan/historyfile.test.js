a70d38a4310389781c14ea0e9b4d6759e183320c
csmig <33138761+csmig@users.noreply.github.com>
2024-02-22 19:41:45 +0000

    feat: in scan mode, migrate addToHistory() calls to the queue handlers (#93)
    Co-authored-by: matte22 <mferreira1264@gmail.com>

--- test/scan/historyfile.test.js
+++ test/scan/historyfile.test.js
@@ -1,17 +1,16 @@
 import { expect } from 'chai'
-import sinon from 'sinon'
 import fs from 'fs'
-import {
-  startScanner,
-  initHistory,
-  addToHistory,
-  removeFromHistory
-} from '../../lib/scan.js'
+import { initHistory, addToHistory, removeFromHistory } from '../../lib/scan.js'
 import { logger } from '../../lib/logger.js'
 import path from 'path'
 import { options } from '../../lib/args.js'
 
-function setOptions(o) {
+const emptyFn = () => undefined
+for (const method of  ['info', 'warn', 'error', 'verbose', 'debug', 'silly', 'http']) {
+  logger[method] = emptyFn
+}
+
+function setOptions (o) {
   for (const [key, value] of Object.entries(o)) {
     options[key] = value
   }
@@ -21,16 +20,12 @@ describe('testing add/remove/init functions  ', function () {
   const historyFile = './watcher.test.history'
   const scannedPath = './test/testFiles/TestScannedDirectory'
 
-  let logStub
-
   beforeEach(function () {
     fs.writeFileSync(historyFile, '')
-    logStub = sinon.stub(logger, 'info')
   })
 
   afterEach(function () {
     fs.unlinkSync(historyFile)
-    logStub.restore()
   })
 
   it('should correctly create an empty history file', async function () {
@@ -41,9 +36,7 @@ describe('testing add/remove/init functions  ', function () {
       oneShot: true,
       historyWriteInterval: 10
     })
-
     initHistory()
-
     expect(fs.existsSync(historyFile)).to.be.true
     expect(fs.readFileSync(historyFile, 'utf8')).to.equal('')
   })
@@ -63,22 +56,19 @@ describe('testing add/remove/init functions  ', function () {
 
     addToHistory(file)
 
-    await new Promise(resolve =>
-      setTimeout(resolve, options.historyWriteInterval)
-    )
-
+    await new Promise(resolve => setTimeout(resolve, options.historyWriteInterval))
     const data = fs.readFileSync(historyFile, 'utf8').trim() // Trim the newline character
     expect(data).to.equal(file)
   })
 
   it('should correctly remove from history file', async function () {
-    const options = {
+    setOptions({
       historyFile: historyFile,
       path: scannedPath,
       scanInterval: 5000,
       oneShot: true,
       historyWriteInterval: 10
-    }
+    })
 
     initHistory(options)
 
@@ -86,15 +76,11 @@ describe('testing add/remove/init functions  ', function () {
 
     addToHistory(file)
 
-    await new Promise(resolve =>
-      setTimeout(resolve, options.historyWriteInterval)
-    )
+    await new Promise(resolve => setTimeout(resolve, options.historyWriteInterval))
 
     removeFromHistory(file)
 
-    await new Promise(resolve =>
-      setTimeout(resolve, options.historyWriteInterval)
-    )
+    await new Promise(resolve => setTimeout(resolve, options.historyWriteInterval))
 
     const data = fs.readFileSync(historyFile, 'utf8').trim()
 
@@ -106,18 +92,12 @@ describe('testing starting with empty history file and adding entries', function
   const historyFile = './watcher.test.history'
   const scannedPath = './test/testFiles/'
   const fileContents = '<?xml version="1.0" encoding="UTF-8"?>'
-  let logStub, warnStub
-
   beforeEach(function () {
     fs.writeFileSync(historyFile, '')
-
     fs.mkdirSync(scannedPath, { recursive: true })
     for (let i = 1; i <= 5; i++) {
       fs.writeFileSync(path.join(scannedPath, `file${i}.ckl`), fileContents)
     }
-
-    logStub = sinon.stub(logger, 'info')
-    warnStub = sinon.stub(logger, 'warn')
   })
 
   afterEach(function () {
@@ -128,9 +108,6 @@ describe('testing starting with empty history file and adding entries', function
       fs.unlinkSync(path.join(scannedPath, file))
     }
     fs.rmSync(scannedPath, { recursive: true })
-
-    logStub.restore()
-    warnStub.restore()
   })
 
   it('should correctly identify the 5 new files and update the history file with all 5', async function () {
@@ -145,82 +122,12 @@ describe('testing starting with empty history file and adding entries', function
     // create history file
     initHistory()
 
-    // start scanning
-    await startScanner()
-
-    await new Promise(resolve =>
-      setTimeout(resolve, options.historyWriteInterval)
-    )
-
-    // read the history file
-    const data = fs.readFileSync(historyFile, 'utf8')
-    const lines = data.split('\n').filter(line => line.trim() !== '')
-
-    expect(lines.length).to.equal(5)
-
-    const expectedHistoryEntries = [
-      './test/testFiles/file1.ckl',
-      './test/testFiles/file2.ckl',
-      './test/testFiles/file3.ckl',
-      './test/testFiles/file4.ckl',
-      './test/testFiles/file5.ckl'
-    ]
-
-    for (const entry of expectedHistoryEntries) {
-      expect(lines).to.include(entry)
-    }
-  })
-})
-
-describe('testing starting with entries in history file that are also in the scanned directory', function () {
-  const historyFile = './watcher.test.history'
-  const scannedPath = './test/testFiles/'
-  const fileContents = '<?xml version="1.0" encoding="UTF-8"?>'
-
-  let logStub, warnStub
-
-  beforeEach(function () {
-    fs.writeFileSync(
-      historyFile,
-      './test/testFiles/TestScannedDirectory/file1.ckl\n./test/testFiles/TestScannedDirectory/file2.ckl\n'
-    )
-
-    fs.mkdirSync(scannedPath, { recursive: true })
-    for (let i = 1; i <= 5; i++) {
-      fs.writeFileSync(path.join(scannedPath, `file${i}.ckl`), fileContents)
-    }
-
-    logStub = sinon.stub(logger, 'info')
-    warnStub = sinon.stub(logger, 'warn')
-  })
-
-  afterEach(function () {
-    fs.unlinkSync(historyFile)
-
+ 
     const files = fs.readdirSync(scannedPath)
     for (const file of files) {
-      fs.unlinkSync(path.join(scannedPath, file))
+      addToHistory(path.join(scannedPath, file))
     }
-    fs.rmSync(scannedPath, { recursive: true })
 
-    logStub.restore()
-    warnStub.restore()
-  })
-
-  it('should correctly return the two files previously in the history file aswell as the other three in the scanned directory.', async function () {
-    setOptions({
-      historyFile: historyFile,
-      path: scannedPath,
-      scanInterval: 5000,
-      oneShot: true,
-      historyWriteInterval: 10
-    })
-
-    // create history file
-    initHistory()
-
-    // start scanning
-    await startScanner()
 
     await new Promise(resolve =>
       setTimeout(resolve, options.historyWriteInterval)
@@ -233,155 +140,11 @@ describe('testing starting with entries in history file that are also in the sca
     expect(lines.length).to.equal(5)
 
     const expectedHistoryEntries = [
-      './test/testFiles/file1.ckl',
-      './test/testFiles/file2.ckl',
-      './test/testFiles/file3.ckl',
-      './test/testFiles/file4.ckl',
-      './test/testFiles/file5.ckl'
-    ]
-
-    for (const entry of expectedHistoryEntries) {
-      expect(lines).to.include(entry)
-    }
-  })
-})
-
-describe('testing starting with entries in history file that are NOT in the scanned directory', function () {
-  const historyFile = './watcher.test.history'
-  const scannedPath = './test/testFiles/'
-  const fileContents = '<?xml version="1.0" encoding="UTF-8"?>'
-
-  let logStub, warnStub
-
-  beforeEach(function () {
-    fs.writeFileSync(
-      historyFile,
-      './test/testFiles//file55.ckl\n./test/testFiles//file22.ckl\n'
-    )
-
-    fs.mkdirSync(scannedPath, { recursive: true })
-    for (let i = 1; i <= 5; i++) {
-      fs.writeFileSync(path.join(scannedPath, `file${i}.ckl`), fileContents)
-    }
-
-    logStub = sinon.stub(logger, 'info')
-    warnStub = sinon.stub(logger, 'warn')
-  })
-
-  afterEach(function () {
-    fs.unlinkSync(historyFile)
-    const files = fs.readdirSync(scannedPath)
-    for (const file of files) {
-      fs.unlinkSync(path.join(scannedPath, file))
-    }
-    fs.rmSync(scannedPath, { recursive: true })
-    logStub.restore()
-    warnStub.restore()
-  })
-
-  it('should correctly remove the 2 files history file then write the 5 in the scanned directory.', async function () {
-    setOptions({
-      historyFile: historyFile,
-      path: scannedPath,
-      scanInterval: 5000,
-      oneShot: true,
-      historyWriteInterval: 10
-    })
-
-    // create history file
-    initHistory()
-
-    // start scanning
-    await startScanner()
-
-    await new Promise(resolve =>
-      setTimeout(resolve, options.historyWriteInterval)
-    )
-
-    // read the history file
-    const data = fs.readFileSync(historyFile, 'utf8')
-    const lines = data.split('\n').filter(line => line.trim() !== '')
-
-    expect(lines.length).to.equal(5)
-
-    const expectedHistoryEntries = [
-      './test/testFiles/file1.ckl',
-      './test/testFiles/file2.ckl',
-      './test/testFiles/file3.ckl',
-      './test/testFiles/file4.ckl',
-      './test/testFiles/file5.ckl'
-    ]
-
-    for (const entry of expectedHistoryEntries) {
-      expect(lines).to.include(entry)
-    }
-  })
-})
-
-describe('testing starting with entries in history file that are not in the scanned directory and files that are in the scanned directory', function () {
-  const historyFile = './watcher.test.history'
-  const scannedPath = './test/testFiles/'
-  const fileContents = '<?xml version="1.0" encoding="UTF-8"?>'
-
-  let logStub, warnStub
-
-  beforeEach(function () {
-    fs.writeFileSync(
-      historyFile,
-      './test/testFiles/TestScannedDirectory/file55.ckl\n./test/testFiles/TestScannedDirectory/file22.ckl\n\n./test/testFiles/TestScannedDirectory/file1.ckl\n./test/testFiles/TestScannedDirectory/file2.ckl\n'
-    )
-
-    fs.mkdirSync(scannedPath, { recursive: true })
-    for (let i = 1; i <= 5; i++) {
-      fs.writeFileSync(path.join(scannedPath, `file${i}.ckl`), fileContents)
-    }
-
-    logStub = sinon.stub(logger, 'info')
-    warnStub = sinon.stub(logger, 'warn')
-  })
-
-  afterEach(function () {
-    fs.unlinkSync(historyFile)
-    const files = fs.readdirSync(scannedPath)
-    for (const file of files) {
-      fs.unlinkSync(path.join(scannedPath, file))
-    }
-    fs.rmSync(scannedPath, { recursive: true })
-    logStub.restore()
-    warnStub.restore()
-  })
-
-  it('should correctly remove the 2 files history file skip 2 files already in the history file and scanned directory and add one file to the history file', async function () {
-    setOptions({
-      historyFile: historyFile,
-      path: scannedPath,
-      scanInterval: 5000,
-      oneShot: true,
-      historyWriteInterval: 50
-    })
-
-    // create history file
-    initHistory()
-
-    // start scanning
-    await startScanner()
-
-    await new Promise(resolve =>
-      setTimeout(resolve, options.historyWriteInterval)
-    )
-
-    // read the history file
-    const data = fs.readFileSync(historyFile, 'utf8')
-    const lines = data.split('\n').filter(line => line.trim() !== '')
-
-    expect(lines.length).to.equal(5)
-
-    const expectedHistoryEntries = [
-      './test/testFiles/file1.ckl',
-      './test/testFiles/file2.ckl',
-      './test/testFiles/file3.ckl',
-      './test/testFiles/file4.ckl',
-      './test/testFiles/file5.ckl'
+      'test/testFiles/file1.ckl',
+      'test/testFiles/file2.ckl',
+      'test/testFiles/file3.ckl',
+      'test/testFiles/file4.ckl',
+      'test/testFiles/file5.ckl'
     ]
 
     for (const entry of expectedHistoryEntries) {
@@ -394,18 +157,13 @@ describe('testing starting with empty history file and slowly adding and removin
   this.timeout(5000)
   const historyFile = './watcher.test.history'
   const scannedPath = './test/testFiles/'
-  let logStub, warnStub
 
   beforeEach(function () {
     fs.writeFileSync(historyFile, '')
-    logStub = sinon.stub(logger, 'info')
-    warnStub = sinon.stub(logger, 'warn')
   })
 
   afterEach(function () {
     fs.unlinkSync(historyFile)
-    logStub.restore()
-    warnStub.restore()
   })
 
   it('should correctly remove the 2 files history file skip 2 files already in the history file and scanned directory and add one file to the history file', async function () {
@@ -423,9 +181,7 @@ describe('testing starting with empty history file and slowly adding and removin
     addToHistory('./test/testFiles/file1.ckl')
     addToHistory('./test/testFiles/file2.ckl')
 
-    await new Promise(resolve =>
-      setTimeout(resolve, options.historyWriteInterval)
-    )
+    await new Promise(resolve => setTimeout(resolve, options.historyWriteInterval))
 
     // read the history file
     const data = fs.readFileSync(historyFile, 'utf8')
@@ -437,9 +193,7 @@ describe('testing starting with empty history file and slowly adding and removin
 
     removeFromHistory('./test/testFiles/file1.ckl')
 
-    await new Promise(resolve =>
-      setTimeout(resolve, options.historyWriteInterval)
-    )
+    await new Promise(resolve => setTimeout(resolve, options.historyWriteInterval))
 
     // read the history file
     const data2 = fs.readFileSync(historyFile, 'utf8')
@@ -453,18 +207,6 @@ describe('testing starting with empty history file and slowly adding and removin
 describe('testing no history file mode', function () {
   const historyFile = null
   const scannedPath = './test/testFiles/'
-
-  let logStub, warnStub
-
-  beforeEach(function () {
-    logStub = sinon.stub(logger, 'info')
-    warnStub = sinon.stub(logger, 'warn')
-  })
-
-  afterEach(function () {
-    logStub.restore()
-    warnStub.restore()
-  })
 
   it('should correctly run in no history file mode', async function () {
     setOptions({
@@ -481,9 +223,7 @@ describe('testing no history file mode', function () {
 
     addToHistory(file)
 
-    await new Promise(resolve =>
-      setTimeout(resolve, options.historyWriteInterval)
-    )
+    await new Promise(resolve => setTimeout(resolve, options.historyWriteInterval))
 
     // expect no history file to be created
     expect(fs.existsSync(historyFile)).to.be.false
@@ -493,9 +233,9 @@ describe('testing no history file mode', function () {
 describe('cleaning up', function () {
   after(async function () {
     setTimeout(() => {
-      process.exit(0); // Delayed exit to allow Mocha to output results
-    }, 1000); // Adjust time as necessary for your environment
-  });
+      process.exit(0) // Delayed exit to allow Mocha to output results
+    }, 1000) // Adjust time as necessary for your environment
+  })
 
   it('should clean up the history file', function () {
     const historyFilePath = './watcher.test.history'
