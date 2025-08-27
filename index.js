b2a6a9c89a14e2640901fd3a7a361499aac6b297
Matte22 <mferreira1264@gmail.com>
2025-08-27 15:13:36 -0400

    feat: watcher will pause when stig manager is in maintenance mode.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { options, configValid }  from './lib/args.js'
 import * as CONSTANTS from './lib/consts.js'
 const minApiVersion = CONSTANTS.MIN_API_VERSION
 const component = 'index'
+let currentMode
 if (!configValid) {
   logger.error({ component, message: 'invalid configuration... Exiting'})
   logger.end()
@@ -16,7 +17,6 @@ import { serializeError } from 'serialize-error'
 import { initScanner } from './lib/scan.js'
 import semverGte from 'semver/functions/gte.js'
 import Alarm from './lib/alarm.js'
-
 
 
 process.on('SIGINT', () => {
@@ -59,9 +59,8 @@ async function run() {
       pid: process.pid,
       options: getObfuscatedConfig(options)
     })
-    
-    await preflightServices()
     setupAlarmHandlers()
+    await preflightServices()
     if (options.mode === 'events') {
       startFsEventWatcher()
     }
@@ -76,7 +75,23 @@ async function run() {
   }
 }
 
+/**
+ * Waits until the API returns to normal mode.
+ * This function will not resolve until `alarmLowered` fires.
+ */
+async function apiNotNormalGate() {
+  return new Promise((resolve) => {
+    const onLowered = (alarmType) => {
+      if (alarmType === 'apiNotNormal') {
+        resolve()
+      }
+    }
+    Alarm.on('alarmLowered', onLowered, { once: true})
+  })
+}
+
 function setupAlarmHandlers() {
+
   const alarmHandlers = {
     apiOffline: api.offlineRetryHandler,
     authOffline: auth.offlineRetryHandler,
@@ -123,7 +138,87 @@ async function hasMinApiVersion () {
   }
 }
 
+// count of EventSource reconnect attempts
+let esRetryCount = 0
+const esRetryLimit = 5
+
+/**
+ * Initializes the EventSource connection to the Stig Manager API server state event stream.
+ * Listens for 'state-report' and 'mode-changed' events to monitor the API mode.
+ * Raises or lowers the 'apiNotNormal' alarm based on the current mode.
+ */
+async function initEventSource() {
+  const es = await api.getAPIEventStream()
+  // Log when connection is established
+  es.onopen = () => logger.info({ component, message: 'Connected to Stig Manager API server state event' })
+
+  // Log and handle connection errors
+  es.onerror = (e) => {
+    logger.error({ component, message: 'Failed to connect to Stig Manager API state server side event.', error: e })
+    esRetryCount++
+      if (esRetryCount > esRetryLimit) {
+        logger.error({ component, message: `Stig Manager API event stream reconnect retry limit reached (${esRetryLimit}).` })
+        Alarm.shutdown(CONSTANTS.ERR_APIOFFLINE)
+      }
+  }
+
+  
+  /**
+   * Returns a Promise that resolves with the initial state payload received from a 'state-report' event.
+   * 
+   * The function listens for a single 'state-report' event from the EventSource `es`, parses the event data as JSON,
+   * logs the initial state, updates the current mode, triggers an alarm if the mode is not 'normal', and then resolves
+   * the Promise with the parsed payload.
+   *
+   */
+ const getInitialState = new Promise((resolve) => {
+    const onStart = (e) => {
+      let payload
+      try { 
+        payload = JSON.parse(e.data)
+      }
+      catch (err) {
+        logger.error({ component, message: 'Failed to get Stig Manager API mode.', error: err })
+        return
+      }
+      logger.info({ component, message: 'Stig Manager Initial State Report', state: payload })
+      currentMode = payload?.mode?.currentMode
+      Alarm.apiNotNormal(payload?.mode?.currentMode !== 'normal')
+      es.removeEventListener('state-report', onStart)
+      resolve(payload)
+    }
+    es.addEventListener('state-report', onStart, { once: true })
+  })
+  
+  // Handler for 'state-report' and 'mode-changed' events
+  const handleEvent = (e) => {
+    let payload 
+    try {
+      payload = JSON.parse(e.data)
+    }
+    catch (err) {
+      logger.error({ component, message: 'Failed to get Stig Manager API mode.', error: err })
+      return
+    }
+    if(payload?.mode?.currentMode !== currentMode) {
+      currentMode = payload?.mode?.currentMode
+      logger.info({ component, message: 'Stig Manager Mode Changed', state: payload })
+    }
+    Alarm.apiNotNormal(payload?.mode?.currentMode !== 'normal')
+  }
+  // wait for the initial state before adding event listeners
+  await getInitialState
+
+  es.addEventListener('state-report', handleEvent)
+  es.addEventListener('mode-changed', handleEvent)
+}
+
 async function preflightServices () {
+  await initEventSource()
+  // block here until api is in normal mode so we continue with preflights 
+  if (Alarm.isAlarmed()) {
+    await apiNotNormalGate()
+  }
   await hasMinApiVersion()
   await auth.getOpenIDConfiguration()
   await auth.getToken()
